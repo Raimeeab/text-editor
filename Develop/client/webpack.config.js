c3fcf0ae1c23327c6df782520212813cd10ad70a
Raimeeab <raimee.abbassi@gmail.com>
2022-01-11 15:14:28 +1100

    Begin webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -30,7 +30,20 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false, 
         inject: true, 
-        name: "JATE"
+        name: "Text Editor",
+        short_name: "JATE", 
+        description: "A single-page text-editor application that runs in the browser, featuring a number of data presistence techniques and also functions offline.",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
     ],
 
